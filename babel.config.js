3ee2afc496d3117f16aabb8db580dfe04963a188
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-06 21:28:52 +0200

    Make needed changes to configs

--- babel.config.js
+++ babel.config.js
@@ -1,22 +1,23 @@
 module.exports = {
   plugins: [
     [
-      '@babel/plugin-proposal-decorators',
+      "@babel/plugin-proposal-decorators",
       {
         legacy: true,
       },
     ],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "@babel/plugin-syntax-jsx",
   ],
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
         targets: {
-          node: 'current',
+          node: "current",
         },
       },
     ],
-    '@babel/react',
+    "@babel/react",
   ],
 };
