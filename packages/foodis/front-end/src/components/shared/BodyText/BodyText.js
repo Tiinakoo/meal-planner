2cf0b1115c33e0f6800499fb63e0bfefccfeaa51
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-14 18:53:07 +0200

    Introduce a component for body text

---
+++ packages/foodis/front-end/src/components/shared/BodyText/BodyText.js
@@ -0,0 +1,21 @@
+import React from "react";
+import upperFirst from "lodash/fp/upperFirst";
+import styles from "./BodyText.module.scss";
+import classNames from "classnames";
+
+const BodyText = ({ size, font, className, children, ...props }) => {
+  const classes = classNames([
+    styles.bodyText,
+    styles[`bodyText__${font}`],
+    styles[`bodyText__size${upperFirst(size)}`],
+    className,
+  ]);
+
+  return (
+    <p className={classes} {...props}>
+      {children}
+    </p>
+  );
+};
+
+export default BodyText;
