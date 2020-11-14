243c884cdac0084e2c33efcede49cd66506c5025
Tiina Kimari <tiina.kimari@houston-inc.com>
2020-11-14 18:59:37 +0200

    Add label for creating a department

--- packages/foodis/front-end/src/departments/CreateDepartment.js
+++ packages/foodis/front-end/src/departments/CreateDepartment.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import TextInput from "../components/shared/TextInput/TextInput";
+import BodyText from "../components/shared/BodyText/BodyText";
 
 const CreateDepartment = observer(({ createDepartmentModel }) => (
   <form>
+    <BodyText font="primary" size="md">
+      Add new department
+    </BodyText>
     <TextInput model={createDepartmentModel.name} />
   </form>
 ));
